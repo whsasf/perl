52e912e0b6148b29374c348184fbdbd6e69690c0
whsasf <whsasf@126.com>
2017-12-07 13:48:20 +0800

    13

--- chapter13/13-8.pl
+++ chapter13/13-8.pl
@@ -1,10 +1,9 @@
 #!/usr/bin/perl -w
 use strict;
-use autodie;
 
 
 chdir $ARGV[0];
-foreach (glob '*'){
-	my $dest=readlink $_;
+foreach (<*> ){
+	my $dest = readlink $_;
 	print "$_ -> $dest\n" if defined $dest;
 	}
